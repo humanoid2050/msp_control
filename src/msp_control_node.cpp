00b87cc56b06e0f75c8f40987cb8478d40136fc5
humanoid2050 <humanoid2050@katana>
2019-01-05 17:43:07 -0500

    reduced output

--- src/msp_control_node.cpp
+++ src/msp_control_node.cpp
@@ -24,13 +24,6 @@
 
 #include <signal.h>
 
-void mySigintHandler(int sig)
-{
-    std::cout << "signal caught" <<std::endl;
-    ros::shutdown();
-    std::cout << "sent shutdown" <<std::endl;
-}
-
 
 using msp_control::FlightMode;
 using utilities::platform_control;
@@ -38,19 +31,6 @@ using utilities::platform_control;
 class App {
 public:
 
-    //enum SOURCE {UNSET, MSP, SBUS, OTHER};
-    //enum FLIGHTMODE {ANGLE, NAV_HOLD, RTH};
-    /*
-    enum rxReceiverType_e {
-        RX_TYPE_NONE        = 0,
-        RX_TYPE_PWM         = 1,
-        RX_TYPE_PPM         = 2,
-        RX_TYPE_SERIAL      = 3,
-        RX_TYPE_MSP         = 4,
-        RX_TYPE_SPI         = 5,
-        RX_TYPE_UIB         = 6
-    } ;
-    */
     App(ros::NodeHandle& n) : node_(n)//, armed_(false), altitude_hold_(false) // mode_(ANGLE), 
     {
         imu_pub = node_.advertise<sensor_msgs::Imu>("imu", 10);
@@ -60,11 +40,18 @@ public:
     }
 
     void onStatus(msp::msg::InavStatus& status) {
-        std::cout<<status;
+        //std::cout << status;
+        std::string arming_flags = msp::armingFlagToString(status.arming_flags());
+        
+        if (arming_flags != arming_flags_) {
+            arming_flags_ = arming_flags;
+            std::cout << arming_flags << std::endl;
+        }
+        
     }
     
     void onRc(msp::msg::Rc& rc) {
-        std::cout << rc;
+        //std::cout << rc;
     }
 
     void onImu(msp::msg::RawImu& imu_raw) {
@@ -136,88 +123,6 @@ public:
         alt.value = altitude.altitude();
         alt_pub.publish(alt);
     }
-    /*
-    void arm()
-    {
-        armed_ = true;
-    }
-    
-    void disarm()
-    {
-        armed_ = false;
-    }
-    
-    bool isArmed()
-    {
-        return armed_;
-    }
-    
-    void setAltitudeHold(bool on)
-    {
-        altitude_hold_ = on;
-    }
-    
-    bool getAltitudeHold()
-    {
-        return altitude_hold_;
-    }
-    
-    
-    void setSource(fcu::ControlSource src)
-    {
-        source_ = src;
-    }
-    
-    fcu::ControlSource getSource()
-    {
-        return source_;
-    }
-    
-    void setMode(FLIGHTMODE mode)
-    {
-        mode_ = mode;
-    }
-    
-    FLIGHTMODE getMode()
-    {
-        return mode_;
-    }
-    
-    std::vector<uint16_t> inputControl(const platform_control& control)
-    {
-        
-        
-        //std::cout << "std::vector<uint16_t> inputControl(const platform_control& control)" << std::endl;
-        if (control.commandType != platform_control::TYPE_QUAD_SIMPLE) return std::vector<uint16_t>();
-        
-        std::vector<uint16_t> cmds(18,1000);
-        //manually remapping from EATR (platform control sequence) to TAER (american RC)
-        
-        cmds[0] = (control.value[2]*500)+1500;
-        cmds[1] = (control.value[1]*500)+1500;
-        cmds[2] = (control.value[0]*500)+1500;
-        cmds[3] = (control.value[3]*500)+1500;
-        
-        if (!armed_) cmds[4] = 1000;
-        else if (altitude_hold_) cmds[4] = 2000;
-        else cmds[4] = 2000;
-        
-        switch (mode_) {
-        case ANGLE:
-            cmds[5] = 1000;
-            break;
-        case NAV_HOLD:
-            cmds[5] = 1500;
-            break;
-        case RTH:
-            cmds[5] = 2000;
-            break;
-        }
-        
-        return cmds;
-    }
-    */
-    
 private:
     ros::NodeHandle& node_;
     ros::Publisher imu_pub;
@@ -225,12 +130,9 @@ private:
     ros::Publisher alt_pub;
     ros::Publisher nav_pub;
     
-    tf2::Quaternion orientation_;
+    std::string arming_flags_;
     
-    //fcu::ControlSource source_;
-    //FLIGHTMODE mode_;
-    //bool armed_;
-    //bool altitude_hold_;
+    tf2::Quaternion orientation_;
     
 };
 
@@ -258,30 +160,29 @@ int main(int argc, char **argv)
 
     App app(n);
     
-    fcu.subscribe(&App::onStatus, &app, 0.1);
-    std::this_thread::sleep_for(std::chrono::milliseconds(10));
+    fcu.subscribe(&App::onStatus, &app, 0.5);
+    std::this_thread::sleep_for(std::chrono::milliseconds(1));
     fcu.subscribe(&App::onImu, &app, 0.1);
-    std::this_thread::sleep_for(std::chrono::milliseconds(10));
+    std::this_thread::sleep_for(std::chrono::milliseconds(1));
     fcu.subscribe(&App::onGps, &app, 0.1);
-    std::this_thread::sleep_for(std::chrono::milliseconds(10));
-    fcu.subscribe(&App::onCompGps, &app, 0.1);
-    std::this_thread::sleep_for(std::chrono::milliseconds(10));
-    fcu.subscribe(&App::onGpsStatistics, &app, 0.1);
-    std::this_thread::sleep_for(std::chrono::milliseconds(10));
+    std::this_thread::sleep_for(std::chrono::milliseconds(1));
+    //fcu.subscribe(&App::onCompGps, &app, 0.1);
+    //std::this_thread::sleep_for(std::chrono::milliseconds(1));
+    //fcu.subscribe(&App::onGpsStatistics, &app, 0.1);
+    //std::this_thread::sleep_for(std::chrono::milliseconds(1));
     fcu.subscribe(&App::onAttitude, &app, 0.1);
-    std::this_thread::sleep_for(std::chrono::milliseconds(10));
+    std::this_thread::sleep_for(std::chrono::milliseconds(1));
     fcu.subscribe(&App::onAltitude, &app, 0.1);
-    std::this_thread::sleep_for(std::chrono::milliseconds(10));
+    //std::this_thread::sleep_for(std::chrono::milliseconds(1));
     //fcu.subscribe(&App::onRc, &app, 0.1);
     
     //handle control messages
     boost::function<void (const platform_control&)> setRcValues = 
         [&] (const platform_control& message) { 
             if (message.commandType != platform_control::TYPE_QUAD_SIMPLE) return;
-            std::cout << "-> platform control: " << std::endl;
+
             std::array<double,4> rpyt = {message.value[1],message.value[0],message.value[3],message.value[2]};
             fcu.setRPYT(rpyt);
-            std::cout << "-> platform control finished" << std::endl;
         };
     ros::Subscriber control_sub = n.subscribe<platform_control>("control", 1, setRcValues);
     
@@ -289,13 +190,12 @@ int main(int argc, char **argv)
     //handle flight mode messages
     boost::function<void (const FlightMode&)> setMode =
         [&] (const FlightMode& flight_mode) {
-            std::cout << "-> set Flight Mode " << flight_mode.primary_mode << " " << flight_mode.secondary_mode << " " << flight_mode.modifier << std::endl;
             fcu::FlightMode current_fm;
             current_fm.primary = fcu::FlightMode::PRIMARY_MODE(flight_mode.primary_mode);
             current_fm.secondary = fcu::FlightMode::SECONDARY_MODE(flight_mode.secondary_mode);
             current_fm.modifier = fcu::FlightMode::MODIFIER(flight_mode.modifier);
             fcu.setFlightMode(current_fm);
-            std::cout << "-> set Flight Mode finished" << std::endl;
+            
         };
     ros::Subscriber flight_mode_sub = n.subscribe<FlightMode>("flight_mode", 1, setMode );
     
@@ -303,14 +203,15 @@ int main(int argc, char **argv)
     //handle source selection messages
     boost::function<void (const utilities::BoolStamped&)> binarySetSource =
         [&] (const utilities::BoolStamped& use_msp) { 
-            std::cout << "-> setting source from binary" << std::endl;
+            
             fcu.setControlSource( use_msp.value ? fcu::ControlSource::MSP : fcu::ControlSource::SBUS ); 
-            std::cout << "-> setting source from binary finished" << std::endl;
+
         };
     ros::Subscriber control_source_sub = n.subscribe<utilities::BoolStamped>("control_source", 10 , binarySetSource);
     
     
     std::cout << "STARTED MSP NODE SPIN" << std::endl;
+    //fcu.printDebug();
     
     ros::spin();
     std::cout << "LOOP FINISHED" <<std::endl;
